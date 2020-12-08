d5f8dc9ffaf40ac97cb1e5ea186a4ea7e5ca68a5
Jared Mosher <jared.mosher@gmail.com>
2020-12-08 00:39:33 +0000

    Cotinued adding additional functionality to project based on requirements.

--- express_server.js
+++ express_server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -25,6 +31,15 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+}); 
+
+app.post("/urls", (req, res) => {
+  let returnString = generateRandomString();  // Log the POST request body to the console
+  res.send(returnString);         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
@@ -33,11 +48,6 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
